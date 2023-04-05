e3dd860d1bb2e24659e6c0597e433e97929b4254
Abdela umer <adheremo65@gmail.com>
2023-04-04 22:40:57 -0400

    find method is done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function superbowlWin(x){
+    let ball = null; 
+      for(let rec of x){
+          if (rec.result === "W"){
+             let  ball =  rec.year;
+             return ball; 
+            
+          }
+      }
+    
+    
+  } 
+  superbowlWin(record);
